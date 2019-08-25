506ce53a431afe057c44da63266b4334baaa9ef3
summerCat <583188551@qq.com>
2019-08-25 10:57:21 +0800

    Initial commit

---
+++ xxl-job-execute-demo/src/main/java/com/linjingc/xxljobexecutedemo/XxlJobExecuteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.xxljobexecutedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecuteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxlJobExecuteDemoApplication.class, args);
+	}
+
+}
---
+++ xxl-job-execute-demo/src/test/java/com/linjingc/xxljobexecutedemo/XxlJobExecuteDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linjingc.xxljobexecutedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XxlJobExecuteDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
